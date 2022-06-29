56b6955ac4a41bc34b9ce338fdb0bf50ca500b4b
Kevin <kevingtxz@gmail.com>
2022-06-29 10:37:48 -0300

    :construction: Header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-export default function App() {
-  return <div className="App">START</div>;
+import Header from "./component/layout/Header/Header";
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function Main() {
+  return (
+    <>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Routes></Routes>
+        </Suspense>
+      </Router>
+    </>
+  );
 }
---
+++ src/component/layout/Header/Header.jsx
@@ -0,0 +1,30 @@
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => (
+  <header className="header">
+    <Link to="/" className="header-logo header-link-item">
+      PomoTask
+    </Link>
+
+    <nav className="header-nav">
+      <Link to="/" className="header-link-item">
+        Home
+      </Link>
+      <Link to="/tasks" className="header-link-item">
+        Task
+      </Link>
+      {/* <Link to="/statics" className="header-link-item">
+        Static
+      </Link>
+      <Link to="/settings" className="header-link-item">
+        Setting
+      </Link> */}
+      <Link to="/logout" className="header-link-item">
+        Logout
+      </Link>
+    </nav>
+  </header>
+);
+
+export default Header;
